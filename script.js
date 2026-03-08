f8fcfeb6e3a8ffce6d085f429dc5fb8567722ae0
Thomas Correia <thomas.correiap@gmail.com>
2026-03-07 22:35:20 -0500

    Fix duplicate button and add toggle background functionality

--- script.js
+++ script.js
@@ -2,17 +2,23 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
 
+//Fixing the button to change background color and change it back to the original color when clicked again
+const button = document.getElementById("color-button");
+
+let darkMode = false;
+
+button.addEventListener("click", function () {
+
+  if (darkMode === false) {
+    document.body.style.backgroundColor = "#0f172a";
+    darkMode = true;
+  } else {
+    document.body.style.backgroundColor = "#1e3a8a";
+    darkMode = false;
+  }
+
+});
 // Show the current time
 const time = document.createElement("p");
 time.textContent = "Current time: " + new Date().toLocaleTimeString();
 document.body.appendChild(time);
-
-// Create a button
-const button = document.createElement("button");
-button.textContent = "Change Background";
-document.body.appendChild(button);
-
-// When button is clicked, change background color
-button.addEventListener("click", function () {
-  document.body.style.backgroundColor = "#0f172a";
-});
